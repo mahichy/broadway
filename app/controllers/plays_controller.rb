1ecc892cd6ee0f376a5448aa73a3cdf634b3ab02
mahi <mohiddinoshim@yahoo.com>
2021-02-01 20:00:39 +0600

    Problem Category

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -6,17 +6,23 @@ class PlaysController < ApplicationController
 	end
 
 	def new
-		@play = current_user.play.build
+		@play = current_user.plays.build
+		@categories = Category.all.map{ |c| [c.name, c.id]}
 	end
 
 	def show
 	end
 
 	def edit
+		@categories = Category.all.map{ |c| [c.name, c.id]}
 		
 	end
 
 	def update
+		@play.category_id = params[:category_id]
+	
+		
+	
 		if @play.update(play_params)
 			redirect_to play_path(@play)
 		else
@@ -32,6 +38,7 @@ class PlaysController < ApplicationController
 
 	def create
 		@play = current_user.play.build(play_params)
+		@play.category_id = params[:category_id]
 
 		if @play.save
 			redirect_to root_path
@@ -43,11 +50,11 @@ class PlaysController < ApplicationController
 	private
 
 	def play_params
-		params.require(:play).permit(:title, :description, :director)
+		params.require(:play).permit(:title, :description, :director, :category_id)
 	end
 
 	def find_play
 		@play = Play.find(params[:id])	
-		
 	end
+
 end
